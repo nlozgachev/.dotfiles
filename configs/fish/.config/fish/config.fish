41a7f00444ca398483c7fbb436719262b24cf936
Nikita <lozgachev@posteo.net>
2024-03-02 02:24:37 +0400

    fix

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,8 +1,7 @@
-set CURR_FILE $(status --current-filename)
-set CURR_DIR $(dirname $CURR_FILE)
+set CURR_FILE (status --current-filename)
+set CURR_DIR (dirname $CURR_FILE)
 set CONFIGS_DIR $CURR_DIR/config
 
 source $CONFIGS_DIR/set_shell_vars.fish
 source $CONFIGS_DIR/set_aliases.fish
 source $CONFIGS_DIR/set_path.fish
-source $CONFIGS_DIR/init_pyenv.fish
