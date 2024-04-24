5f3663ba90456aa11b500e36156b29cf57f738c3
Nikita Lozgachev <nlozgachev@aligntech.com>
2024-04-24 16:53:40 +0400

    lang envs

--- configs/fish/.config/fish/config/set_path.fish
+++ configs/fish/.config/fish/config/set_path.fish
@@ -4,15 +4,10 @@ fish_add_path $HOME/.volta/bin
 # Deno
 fish_add_path $HOME/.deno/bin
 
-# Go
-fish_add_path $HOME/.go/bin
-# fish_add_path $(go env GOPATH)/bin
-
 # Rust
 fish_add_path $HOME/.cargo/bin
 
-# MacOS Python
-#fish_add_path /usr/local/opt/python@3.10/bin
-
-# Linux Python
-fish_add_path $HOME/.pyenv/bin $PATH
+# Homebrew
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path $HOME/.bin
