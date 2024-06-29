e385417f93ebfdd37c772d5b9dc6869753dfff55
Nikita Lozgachev <nlozgachev@aligntech.com>
2024-06-29 19:04:23 +0400

    chore: update

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -7,18 +7,6 @@ source $CONFIGS_DIR/set_aliases.fish
 source $CONFIGS_DIR/set_path.fish
 source $CONFIGS_DIR/init_pyenv.fish
 source $CONFIGS_DIR/init_goenv.fish
+source $CONFIGS_DIR/init_pnpm.fish
 
 fnm env --use-on-cd | source
-
-source /Users/nlozgachev/.docker/init-fish.sh || true # Added by Docker Desktop
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
-
-
-# pnpm
-set -gx PNPM_HOME "/Users/nlozgachev/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
