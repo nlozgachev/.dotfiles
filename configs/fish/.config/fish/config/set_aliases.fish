1eac676a0e01f309b737e9e603ee7b8fc5fc2f7a
Nikita Lozgachev <code@lozgachev.dev>
2026-03-01 02:06:00 +0400

    chore: revert aliases

---
+++ configs/fish/.config/fish/config/set_aliases.fish
@@ -0,0 +1,37 @@
+alias v=$EDITOR
+alias vim=$EDITOR
+
+alias g="git"
+alias ga="git add"
+alias gaa="git add --all"
+alias gc="git commit -m"
+alias gcm="git commit -m"
+alias gcam="git commit -am"
+alias gst="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gl="git log --oneline --graph --decorate --all"
+alias gp="git push"
+alias gpo="git push origin"
+alias gpl="git pull"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gb="git branch"
+alias gbd="git branch -d"
+alias gcl="git clone"
+
+alias gsu="git submodule update --init --recursive"
+alias grm="git rm --cached"
+alias gfp="git fetch --prune"
+alias grs="git reset --soft HEAD~1"
+alias grh="git reset --hard"
+alias gcp="git cherry-pick"
+alias gbl="git blame"
+alias glf="git log --pretty=format:'%h - %an, %ar : %s'"
+
+# alias dev="just --justfile ~/.config/tmux/justfile dev"
+
+abbr pn pnpm
+abbr pnd pnpm dev
+abbr pns pnpm storybook
+abbr pnt pnpm test:dev
